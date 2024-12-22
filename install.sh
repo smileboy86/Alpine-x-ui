83aecc0d93e7353b6395638d623dd3e2bd6db58a
chrui <smileboy86@gmail.com>
2024-12-22 23:41:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ install_x-ui() {
     rc-service x-ui stop
     cd /usr/local/
 if [ $# == 0 ]; then
-    last_version=$(curl -Ls "https://api.github.com/repos/Lynn-Becky/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    last_version=$(curl -Ls "https://api.github.com/repos/smileboy86/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     if [[ ! -n "$last_version" ]]; then
         echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
         read -p "请手动输入 x-ui 版本号（例如：0.3.4.4）：" last_version
@@ -67,14 +67,14 @@ if [ $# == 0 ]; then
         fi
     fi
     echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
-    wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/Lynn-Becky/Alpine-x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
+    wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/smileboy86/Alpine-x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
     if [[ $? -ne 0 ]]; then
         echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
         exit 1
     fi
 else
     last_version=$1
-    url="https://github.com/Lynn-Becky/Alpine-x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+    url="https://github.com/smileboy86/Alpine-x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
     echo -e "开始安装 x-ui v$1"
     wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
     if [[ $? -ne 0 ]]; then
@@ -91,7 +91,7 @@ fi
     rm x-ui-linux-${arch}.tar.gz -f
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/Lynn-Becky/Alpine-x-ui/main/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/smileboy86/Alpine-x-ui/main/x-ui.sh
     mv /usr/local/x-ui/x-ui.service /etc/init.d/x-ui
     chmod +x /etc/init.d/x-ui
     chmod +x /usr/local/x-ui/x-ui.sh
